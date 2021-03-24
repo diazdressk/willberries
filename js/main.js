ebdfaf43216c81db77545eb95d48b63536e75839
Adilet Abdurasulov <altpen1993@gmail.com>
2021-03-24 22:06:29 +0600

    Add functions for show filtered goods

--- js/main.js
+++ js/main.js
@@ -11,35 +11,30 @@ const mySwiper = new Swiper('.swiper-container', {
 //cart
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
-const modalClose = document.querySelector('.modal-close');
-const overlay = document.querySelector('.overlay');
 
-const openModal = function() {
+const openModal = () => {
 	modalCart.classList.add('show');
 };
 
-const closeModal = function() {
+const closeModal = () => {
 	modalCart.classList.remove('show');
 };
 
 buttonCart.addEventListener('click', openModal);
-modalClose.addEventListener('click', closeModal);
-
-overlay.addEventListener('click', (e) => {
+modalCart.addEventListener('click', e => {
 	const target = e.target;
-	if (target.classList.contains('modal-close') || target === overlay) {
+	if (target.classList.contains('modal-close') || target === modalCart) {
 		closeModal();
 	}
 });
 
 
 //scroll
-
 // (function() {//скроллер
 // 	const  scrollLinks = document.querySelectorAll('a.scroll-link');//все ссылки с классом
 
 // 	for (let i = 0; i < scrollLinks.length; i += 1) {
-// 		scrollLinks[i].addEventListener('click', (e) => {
+// 		scrollLinks[i].addEventListener('click', e => {
 // 			e.preventDefault();
 // 			const id = scrollLinks[i].getAttribute('href');//#body
 // 			document.querySelector(id).scrollIntoView({
@@ -51,13 +46,29 @@ overlay.addEventListener('click', (e) => {
 // })()
 
 
-{//новый способ самовызывающейся функции
+// {//новый способ самовызывающейся функции
+// 	const  scrollLinks = document.querySelectorAll('a.scroll-link');//все ссылки с классом
+
+// 	for (let i = 0; i < scrollLinks.length; i += 1) {
+// 		scrollLinks[i].addEventListener('click', e => {
+// 			e.preventDefault();
+// 			const id = scrollLinks[i].getAttribute('href');//#body
+// 			document.querySelector(id).scrollIntoView({
+// 				behavior: "smooth",
+// 				block: 'start',
+// 			})
+// 		});
+// 	}
+// }
+
+// 2
+{
 	const  scrollLinks = document.querySelectorAll('a.scroll-link');//все ссылки с классом
 
-	for (let i = 0; i < scrollLinks.length; i += 1) {
-		scrollLinks[i].addEventListener('click', (e) => {
+	for (const scrollLink of scrollLinks) {
+		scrollLink.addEventListener('click', e => {
 			e.preventDefault();
-			const id = scrollLinks[i].getAttribute('href');//#body
+			const id = scrollLink.getAttribute('href');//#body
 			document.querySelector(id).scrollIntoView({
 				behavior: "smooth",
 				block: 'start',
@@ -65,3 +76,89 @@ overlay.addEventListener('click', (e) => {
 		});
 	}
 }
+
+
+
+//view all, goods
+const more = document.querySelector('.more');
+const navigationLink = document.querySelectorAll('.navigation-link');
+const longGoodsList = document.querySelector('.long-goods-list');//скрыт
+
+
+
+//получать товары с сервера
+const getGoods = async function() {
+	const res = await fetch('db/db.json');
+	if (!res.ok) {
+		throw 'Ошибка' + res.status;
+	}
+	return await res.json();
+}
+
+// fetch('db/db.json')
+// 	.then(response => {
+// 		return response.json();
+// 	})
+// 	.then(data => {
+// 		console.log(data);
+// 	})
+
+const createCard = (objCard) => {//создаю карточку
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6';
+	console.log(objCard)
+	card.innerHTML = `
+		<div class="goods-card">
+			${objCard.label ? `<span class="label">${objCard.label}</span>` : ''}
+			<img src="db/${objCard.img}" alt="${objCard.name}" class="goods-image">
+			<h3 class="goods-title">${objCard.name}</h3>
+			<p class="goods-description">${objCard.description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id="${objCard.id}">
+				<span class="button-price">${objCard.price}</span>
+			</button>
+		</div>
+	`;
+	return card;
+};
+
+//показывает карточку
+const renderCards = function (data) {
+	longGoodsList.textContent = '';
+	const cards = data.map(createCard);//каждый пришедший элемент из даты забираю и отдаю её функции, он создаст карточку с данными
+	cards.forEach(card => {//либо форичем
+		longGoodsList.append(card);
+	})
+	// longGoodsList.append(...cards);//либо спред оператором апендю
+	document.body.classList.add('show-goods');
+};
+
+more.addEventListener('click', e => {//открыть все товары
+	e.preventDefault();
+	getGoods().then(renderCards);
+});
+
+
+//фильтр одежды
+const filterCards = (field, value) => {
+	getGoods()
+		.then(function (data) {
+			const filteredGoods = data.filter(function(good) {
+				return good[field] === value;
+			});
+			return filteredGoods;
+		})
+		.then(renderCards);
+};
+
+navigationLink.forEach(link => {
+	link.addEventListener('click', e => { 
+		e.preventDefault();
+		const field = link.dataset.field;
+		const value = link.textContent;
+		if (value === 'All') {
+			return getGoods().then(renderCards);
+		}
+		filterCards(field, value);
+	});
+});
+
